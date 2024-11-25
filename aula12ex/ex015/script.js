b6af3a3dc915b87147ef7de1b8f4da05531048f0
jonatarauber <jonatarauber85@gmail.com>
2024-11-25 06:02:45 -0300

    verificador de idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,50 +1,35 @@
 function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
+    var fAno = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || Number(fano.value) > ano){
+    if(fAno.value.lenght == 0 || Number(fAno.value) > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
-    }else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
+    }else {
+        var fSex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
         var genero = ''
-        
-        if(fsex[0].checked){
+        var img = document.createElement('img')
+        if(fSex[0].checked){
             genero = 'Homen'
             if (idade < 5){
+                genero = 'Bebê'
                 //bebe
-                genero = 'um bebê'
-                img.setAttribute('src', 'imagens/bebeMenino.png')
-            }else if (idade < 13){
+            }else if(idade < 14){
                 //criança
-            }else if (idade < 20){
+            }else if(idade < 20){
                 //adolecente
-            }else if (idade < 30){
+            }else if(idade < 30){
                 //jovem
-            }else if (idade < 70){
+            }else if(idade < 70){
                 //adulto
             }else{
                 //idoso
             }
-        }else if(fsex[1].checked){
-            genero = 'Mulher'
-            if (idade >=0 && idade < 5){
-                //bebe
-            }else if (idade < 13){
-                //criança
-            }else if (idade < 20){
-                //adolecente
-            }else if (idade < 30){
-                //jovem
-            }else if (idade < 70){
-                //adulto
-            }else{
-                //idoso
-            }
+        }else if(fSex[1].checked){
+            genero= 'Mulher'
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-        res.appendChild(img)
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
