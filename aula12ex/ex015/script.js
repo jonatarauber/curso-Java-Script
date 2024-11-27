695d87ef9626020938e7d8a31983aec2b3c100c9
jonatarauber <jonatarauber85@gmail.com>
2024-11-27 06:16:42 -0300

    repetição

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -20,35 +20,45 @@ function verificar(){
                 //bebe
             }else if(idade < 12){
                 genero = 'Criança'
+                img.setAttribute('src' , 'imagens/criancaMenino.png')
                 //criança
             }else if(idade < 20){
                 genero = 'Adolescente'
+                img.setAttribute('src', 'imagens/adolescenteHomen.png')
                 //adolecente
             }else if(idade < 30){
                 genero = 'Jovem homen'
+                img.setAttribute('src' , 'imagens/jovemHomen.png')
                 //jovem
             }else if(idade < 70){
                 genero = 'Homen adulto'
+                img.setAttribute('src' , 'imagens/adultoHomem.png')
                 //adulto
             }else{
                 genero = 'Homen idoso'
+                img.setAttribute('src', 'imagens/idoso.png')
                 //idoso
             }
         }else if(fSex[1].checked){
-           
+           corFundo.style.background='pink'
             if(idade < 5){
                 genero = 'Bebê'
+                img.setAttribute('src', 'imagens/bebeMenina.png')
             }else if(idade < 12){
-                genero = 'Crinaça'
+                genero = 'Criança'
+                img.setAttribute('src', 'imagens/criancaMenina.png')
             }else if(idade < 20){
                 genero = 'Adolescente'
+                img.setAttribute('src', 'imagens/adolescenteMulher.png')
             }else if(idade < 30){
                 genero = 'Jovem mulher'
+                img.setAttribute('src' ,'imagens/jovemMulher.png')
             }else if(idade < 70){
                 genero = 'Mulher adulta'
+                img.setAttribute('src', 'imagens/adultoMulher.png')
             }else {
                 genero = 'Mulher idiosa'
-                
+                img.setAttribute('src', 'imagens/idosa.png')
             }
         }
         res.style.textAlign = 'center'
