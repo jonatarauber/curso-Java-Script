4bd04305f4d264f7e91b617e153eae11f9887ccc
jonatarauber <jonatarauber85@gmail.com>
2024-12-05 06:01:59 -0300

    arrays

--- aula14ex/script2.js
+++ aula14ex/script2.js
@@ -10,6 +10,7 @@ function gerarTab(){
         while(c < 10){
             var item = document.createElement('option')
             item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+console.log(`${num}`)
+num[5]=6
+console.log(`${num}`)
+num.push(7)
+console.log(`${num}`)
