af33ec46bc9f158e45b9bc3161252b07ce6344f3
jonatarauber <jonatarauber85@gmail.com>
2024-12-06 06:01:22 -0300

    for

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,3 +4,12 @@ num[5]=6
 console.log(`${num}`)
 num.push(7)
 console.log(`${num}`)
+num.sort()
+console.log(`${num}`)
+console.log(`O tamanho é ${num.length}`)
+let pos = num.indexOf(9)
+if (pos == -1){
+    console.log('O número não foi encotrado')
+}else{
+    console.log(`O valor está na posição ${pos}`)
+}
