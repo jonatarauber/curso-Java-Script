ea35a03b5b5b9434320a42a3d7e2b9adc89792c8
jonatarauber <jonatarauber85@gmail.com>
2024-12-18 05:52:17 -0300

    exe16

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -3,3 +3,26 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+function isNumero(n){
+    if(Number(n) <= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value , valores)){
+        valores.push(Number(num.value))
+    }else{
+        alert('Valor inválido ou já encontrado na lista')
+    }
+}
