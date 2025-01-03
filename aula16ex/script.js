f14e76cc8afd29f7ac04aee6a068066511265117
jonatarauber <jonatarauber85@gmail.com>
2025-01-03 06:10:15 -0300

    finalizandoProjeto

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -25,6 +25,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} foi adicionado.`
         lista.appendChild(item)
+        res.innerHTML=''
     }else{
         alert('Valor inválido ou já encontrado na lista')
     }
@@ -36,6 +37,24 @@ function finalizar(){
     if(valores.length == 0){
         alert('Adicione valores antes de finalizar!')
     }else{
-        
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior=valores[pos]
+            if(valores[pos] < menor)
+                menor=valores[pos]
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML +=`<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML +=`<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML +=`<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
     }
 }
