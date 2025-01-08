b6021dfb8ac891de795eee18634bd5ff8f9be620
jonatarauber <jonatarauber85@gmail.com>
2025-01-08 05:32:15 -0300

    Create objeto01.js

---
+++ aula17/objeto01.js
@@ -0,0 +1,7 @@
+let amigo = {nome: 'José' , sexo:  'M', peso: 85.4, engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordar()
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
